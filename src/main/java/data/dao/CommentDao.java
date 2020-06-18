e88d54b6027b59a4290bcb31298405b9bd1d4600
cay-kim <63325407+cay-kim@users.noreply.github.com>
2020-06-18 18:37:47 +0900

    댓글 기본기능 ref #15
    커밋 26f57c8 에 Dao 추가

--- src/main/java/data/dao/CommentDao.java
+++ src/main/java/data/dao/CommentDao.java
@@ -1,5 +1,7 @@
 package data.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +13,37 @@ public class CommentDao extends SqlSessionDaoSupport implements CommentDaoInter 
 	@Override
 	public void insertComment(CommentDto cdto) {
 		// TODO Auto-generated method stub
-		
+		getSqlSession().insert("insertOfComment", cdto);
+	}
+
+	@Override
+	public List<CommentDto> getAllComments(String post_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateCommentLikes(String id) {
+		// TODO Auto-generated method stub
+		getSqlSession().update("updateLikesOfComment", id);
+	}
+
+	@Override
+	public String getCommentLikes(String id) {
+		// TODO Auto-generated method stub
+		return getSqlSession().selectOne("likesOfComment", id);
+	}
+
+	@Override
+	public void deleteComment(String id) {
+		// TODO Auto-generated method stub
+		getSqlSession().update("hideOfDeletedComment", id);
+	}
+
+	@Override
+	public void updateComment(CommentDto cdto) {
+		// TODO Auto-generated method stub
+		getSqlSession().update("updateOfComment", cdto);
 	}
 	
 }
