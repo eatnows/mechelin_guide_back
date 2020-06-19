b84289e3ae7744f987bb6c93cb70db2d68892422
cay-kim <63325407+cay-kim@users.noreply.github.com>
2020-06-19 18:23:17 +0900

    카카오, 인증코드메일 ref #16
    카카오API DB연동 수정,
    비밀번호 변경 인증코드 추가

--- src/main/java/kakao/login/SetKakaoApi.java
+++ src/main/java/kakao/login/SetKakaoApi.java
@@ -24,7 +24,7 @@ import com.google.gson.JsonParser;
 public class SetKakaoApi {
 
 	public String getAccessToken(String authorize_code) {
-		String access_Token = "";
+		String access_token = "";
 		String refresh_Token = "";
 		String reqURL = "https://kauth.kakao.com/oauth/token";
 
@@ -64,7 +64,7 @@ public class SetKakaoApi {
 			JsonParser parser = new JsonParser();
 			JsonElement element = parser.parse(result);
 
-			access_Token = element.getAsJsonObject().get("access_token").getAsString();
+			access_token = element.getAsJsonObject().get("access_token").getAsString();
 			refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
 			
 			br.close();
@@ -75,10 +75,10 @@ public class SetKakaoApi {
 			System.out.println("kakao get tokens method error: " + e.getMessage());
 		}
 
-		return access_Token;
+		return access_token;
 	}
 
-	public HashMap<String, Object> getUserInfo(String access_Token) {
+	public HashMap<String, Object> getUserInfo(String access_token) {
 
 		// map 사용: 카카오에도 없는 정보가 있을 수 있음
 		HashMap<String, Object> userInfo = new HashMap<String, Object>();
@@ -90,40 +90,39 @@ public class SetKakaoApi {
 			conn.setRequestMethod("POST");
 
 			// 요청에 필요한 Header에 포함될 내용
-			conn.setRequestProperty("Authorization", "Bearer " + access_Token);
-
+			conn.setRequestProperty("Authorization", "Bearer " + access_token);
+			
+			// 연결 성공 시 200 (콘솔 확인용)
 			int responseCode = conn.getResponseCode();
 			System.out.println("responseCode : " + responseCode);
 
+			// Json 형태의 데이터 String 으로 받기
 			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
 			String line = "";
 			String result = "";
-
 			while ((line = br.readLine()) != null) {
 				result += line;
 			}
 			System.out.println("response body : " + result);
 
+			// String result 를 객체화
 			JsonParser parser = new JsonParser();
 			JsonElement element = parser.parse(result);
-			JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+			
 			JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+			JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
 			
 			String kakaoId = element.getAsJsonObject().get("id").getAsString();
-			
-//			!!!!! email 수집동의여부 묻기 추가바람 !!!!!
-			String email = "empty";
-			if (email != null) {	// 임시코드
-				email = kakao_account.getAsJsonObject().get("email").getAsString();
-			}
-			
 			String nickname = properties.getAsJsonObject().get("nickname").getAsString();
-			String profile_url = properties.getAsJsonObject().get("profile_image").getAsString();
+			String profile_image = properties.getAsJsonObject().get("profile_image").getAsString();
+			String email = kakao_account.getAsJsonObject().get("email").toString();
+			email = email.replace("\"", "");
+			System.out.println("email 따옴표 제거 : " + email);
 			
 			userInfo.put("kakaoId", kakaoId);
 			userInfo.put("nickname", nickname);
-			userInfo.put("profile_url", profile_url);
+			userInfo.put("profile_url", profile_image);
+			userInfo.put("email", email);
 			
 			// map에 element 객체를 통으로 넣으면 받을 때 어떻게 받는지 알아내야함...
 			// rfc 3339 parsing 아직 해결 못 함
@@ -139,13 +138,13 @@ public class SetKakaoApi {
 	}
 	
 	
-	public void kakaoLogout(String access_Token) {
+	public void kakaoLogout(String access_token) {
 		String reqURL = "https://kapi.kakao.com/v1/user/logout";
 	    try {
 	        URL url = new URL(reqURL);
 	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 	        conn.setRequestMethod("POST");
-	        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+	        conn.setRequestProperty("Authorization", "Bearer " + access_token);
 	        
 	        int responseCode = conn.getResponseCode();
 	        System.out.println("responseCode : " + responseCode);
@@ -163,6 +162,22 @@ public class SetKakaoApi {
 	        // TODO Auto-generated catch block
 	        e.printStackTrace();
 	        System.out.println("kakao logout method error: " + e.getMessage());
+	    }
+	}
+	
+	
+	public void deleteUser(String access_token) {
+		String reqURL = "https://kapi.kakao.com/v1/user/unlink";
+	    try {
+	        URL url = new URL(reqURL);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("POST");
+	        conn.setRequestProperty("Authorization", "Bearer " + access_token);
+	        
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	        System.out.println("kakao delete method error: " + e.getMessage());
 	    }
 	}
 
