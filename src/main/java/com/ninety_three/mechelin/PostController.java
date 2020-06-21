b84dfbd66ac80f2cf3b15ef38ebe29bdd0f6603e
eatnows <gagyasindev@gmail.com>
2020-06-21 15:32:34 +0900

    좋아요버튼 매핑주소 수정 fixed #13

--- src/main/java/com/ninety_three/mechelin/PostController.java
+++ src/main/java/com/ninety_three/mechelin/PostController.java
@@ -121,7 +121,6 @@ public class PostController {
 	 */
 	@GetMapping("/review")
 	public List<PostDto> selectDataPost(@RequestParam int user_place_id, @RequestParam int row) {
-		System.out.println(row);
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		map.put("user_place_id", user_place_id);
 		map.put("row", row);
