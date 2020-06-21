f1bb2a248122e57218a2481d08da989e76842a8a
eatnows <gagyasindev@gmail.com>
2020-06-22 01:28:14 +0900

    파일업로드 빈추가 및 데이터 타입 수정 ref #21
    파일이 넘어오는 듯 하나 s3에 어떻게 할것인지는 아직임

--- src/main/java/com/ninety_three/mechelin/ImageController.java
+++ src/main/java/com/ninety_three/mechelin/ImageController.java
@@ -1,11 +1,16 @@
 package com.ninety_three.mechelin;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import data.dao.ImageDaoInter;
 import data.dto.ImageDto;
@@ -22,7 +27,7 @@ public class ImageController {
 	 * image 테이블에 데이터 insert
 	 */
 	@PostMapping("/add")
-	public void insertImage(@RequestBody ImageDto dto) {
-		dao.insertImage(dto);
+	public void insertImage(@RequestParam MultipartFile images) {
+		System.out.println(images.getOriginalFilename());
 	}
 }
