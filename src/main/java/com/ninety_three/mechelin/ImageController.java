f37da88105da5486e4f315d60bf1f250d2c8e8e3
eatnows <gagyasindev@naver.com>
2020-06-22 15:23:58 +0900

    S3에 업로드 성공 ref #21

--- src/main/java/com/ninety_three/mechelin/ImageController.java
+++ src/main/java/com/ninety_three/mechelin/ImageController.java
@@ -1,7 +1,15 @@
 package com.ninety_three.mechelin;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,8 +20,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
 import data.dao.ImageDaoInter;
 import data.dto.ImageDto;
+import data.util.AwsS3;
+
 
 @RestController
 @CrossOrigin
@@ -22,12 +41,42 @@ public class ImageController {
 	
 	@Autowired
 	private ImageDaoInter dao;
+	@Autowired
+	//private AwsS3ImageUpload S3;
+	private AwsS3 s3; 
+	
+	ObjectMetadata metadata = new ObjectMetadata();
+
+	
+	
 	
 	/*
 	 * image 테이블에 데이터 insert
 	 */
 	@PostMapping("/add")
-	public void insertImage(@RequestParam MultipartFile images) {
-		System.out.println(images.getOriginalFilename());
+	public String insertImage(@RequestParam MultipartFile images) {
+		System.out.println("메소드는 실행됨");
+		String bucketName = "버킷이름";
+		String fileName = "testFileFIelfiele.jpg";
+		File file = new File(images.getOriginalFilename());
+		try {
+			file.createNewFile();
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(images.getBytes());
+			fos.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			System.out.println("업로드도됨");
+			String path = s3.fileupload(bucketName, fileName, file);
+			return path;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
---
+++ src/main/java/data/util/AwsS3.java
@@ -0,0 +1,68 @@
+package data.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+@Component
+public class AwsS3 {
+
+    private String accessKey ="액세스키";
+    private String secretKey ="시크릿키";
+
+    private AmazonS3 s3Client;
+
+    private AwsS3() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        ClientConfiguration clientConfig = new ClientConfiguration();
+        clientConfig.setProtocol(Protocol.HTTP);
+        this.s3Client = new AmazonS3Client(credentials, clientConfig);
+        s3Client.setEndpoint("s3.ap-northeast-2.amazonaws.com"); //  아시아 태평양 서울
+    }
+
+
+    public String fileupload(String bucketName,String fileName, File file) throws FileNotFoundException{
+    	System.out.println(bucketName);
+    	System.out.println(fileName);
+    	System.out.println(file);
+        String fileExt = file.getName().split("[.]")[1];
+        this.s3Client.putObject(new PutObjectRequest(bucketName, "images/place/0/"+fileName+"."+fileExt, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        String path = "https://버킷이름.s3.ap-northeast-2.amazonaws.com/"+"images/place/0/"+fileName+"."+fileExt;
+        System.out.println(path);
+        return path;
+    }
+    
+    
+//    public String getS3ObjectContentAsString(String bucketName, String key) {
+//        try {
+//            if (key.startsWith("/")) {
+//                key = key.substring(1);
+//            }
+//            if (key.endsWith("/")) {
+//                key = key.substring(0, key.length());
+//            }
+//            try (InputStream is = s3Client.getObject(bucketName, key).getObjectContent()) {
+//                return StreamUtils.copyToString(is, StandardCharsets.UTF_8);
+//            }
+//        } catch (Exception e) {
+//            throw new IllegalStateException(e);
+//        }
+//    }
+
+
+}
