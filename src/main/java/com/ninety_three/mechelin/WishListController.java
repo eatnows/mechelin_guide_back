d9d0aa74a8e74efe46aa7e18c066d186cd2e489d
eatnows <gagyasindev@naver.com>
2020-06-30 18:59:49 +0900

    postDto에 wishlist_id 추가

--- src/main/java/com/ninety_three/mechelin/WishListController.java
+++ src/main/java/com/ninety_three/mechelin/WishListController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,8 +59,8 @@ public class WishListController {
 	/*
 	 * 삭제 버튼을 눌렀을시 위시 리스트에서 삭제
 	 */
-	@DeleteMapping("/delete")
-	public void deleteWishList(@RequestParam int id) {
+	@DeleteMapping("/delete/{id}")
+	public void deleteWishList(@PathVariable int id) {
 		dao.deleteClickWishList(id);
 	}
 	
